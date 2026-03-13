ea400fc37184e69e321d2657c1684831f07c1b64
gideondakore <dakoregideon72@gmail.com>
2026-03-13 14:09:54 +0000

    chore: firt commit

---
+++ src/main/java/com/example/quickstart/HelloworldController.java
@@ -0,0 +1,13 @@
+package com.example.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloworldController {
+
+    @GetMapping(path = "/hello")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+}
