f8f3301590b7ff4771800f9f9d7778ebe863725c
gideondakore <dakoregideon72@gmail.com>
2026-03-23 21:54:21 +0000

    fix: git conflict in author service implementation

--- src/main/java/com/example/quickstart/services/impl/AuthorServiceImpl.java
+++ src/main/java/com/example/quickstart/services/impl/AuthorServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.quickstart.services.impl;
 import com.example.quickstart.domain.entities.AuthorEntity;
 import com.example.quickstart.repositories.AuthorRepository;
 import com.example.quickstart.services.AuthorService;
-<<<<<<< HEAD
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,21 +14,5 @@ public class AuthorServiceImpl implements AuthorService {
     @Override
     public AuthorEntity createAuthor(AuthorEntity authorEntity) {
         return authorRepository.save(authorEntity);
-=======
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthorServiceImpl implements AuthorService {
-
-    private AuthorRepository authorRepository;
-
-    public AuthorServiceImpl(AuthorRepository authorRepository) {
-        this.authorRepository = authorRepository;
-    }
-
-    @Override
-    public AuthorEntity createAuthor(AuthorEntity authorEntity) {
-       return authorRepository.save(authorEntity);
->>>>>>> origin/web
     }
 }
